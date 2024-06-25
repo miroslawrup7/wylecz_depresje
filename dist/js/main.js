11d88da906f5d657c56a27fa66d631b4f65e2a0d
Mirek Rup <miroslawrup7@gmail.com>
2024-06-25 17:15:01 +0200

    progress bar

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,19 @@
 "use strict";
 
+const inputsRadioArr = document.querySelectorAll("input[type=radio]")
+const progressBarLoc = document.querySelector(".progress-bar")
+const progressTextLoc = document.querySelector(".progress-text")
 
+const calculateRealisation = ()=> {
+    let percent = 
+        Number((document.querySelectorAll("input:checked").length / 
+        document.querySelectorAll("input[type=radio").length * 100).toFixed(1))
+
+    progressBarLoc.style.width = percent + "%"
+    progressTextLoc.innerText = percent + "%"
+}
+
+inputsRadioArr.forEach((elem)=>{
+    elem.addEventListener("change", calculateRealisation)
+})
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,18 @@
 "use strict";
 
+const inputsRadioArr = document.querySelectorAll("input[type=radio]")
+const progressBarLoc = document.querySelector(".progress-bar")
+const progressTextLoc = document.querySelector(".progress-text")
+
+const calculateRealisation = ()=> {
+    let percent = 
+        Number((document.querySelectorAll("input:checked").length / 
+        document.querySelectorAll("input[type=radio").length * 100).toFixed(1))
+
+    progressBarLoc.style.width = percent + "%"
+    progressTextLoc.innerText = percent + "%"
+}
+
+inputsRadioArr.forEach((elem)=>{
+    elem.addEventListener("change", calculateRealisation)
+})
